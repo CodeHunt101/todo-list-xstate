c5920e7bae5dc3f95dfa32c132508fd6432c2ccc
Harold <harold.marino@iag.com.au>
2023-04-13 11:31:38 +1000

    add todo item

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,54 @@
-import { todosMachine } from "@/machines/todoAppMachine"
-import { useMachine } from "@xstate/react"
+import { todosMachine } from "@/machines/todoAppMachine";
+import { useMachine } from "@xstate/react";
+
+const todos = new Set<string>(["Take bins out", "Do laundry"]);
 
 export default function Home() {
-const [state, send] = useMachine(todosMachine, {
-  services: {
-    loadTodos: async () => {
-      return ['Take bins out', 'Do laundry']
-    }
-  }
-})
+  const [state, send] = useMachine(todosMachine, {
+    services: {
+      loadTodos: async () => {
+        return Array.from(todos);
+      },
+      saveTodo: async (context, event) => {
+        todos.add(context.createNewTodoFormInput);
+      },
+    },
+  });
 
   return (
     <div>
       <pre>{JSON.stringify(state.value)}</pre>
       <pre>{JSON.stringify(state.context)}</pre>
       <div>
-        {state.matches('Todos Loaded') && (
-        <button
-          onClick={()=> {
+        {state.matches("Todos Loaded") && (
+          <button
+            onClick={() => {
+              send({
+                type: "Create new",
+              });
+            }}
+          >
+            Create new
+          </button>
+        )}
+        {state.matches("Creating new todo.Showing form input") && (
+          <form onSubmit={(e) => {
+            e.preventDefault()
             send({
-              type: "Create new"
-            })
-          }}
-        >
-          Create new
-        </button>)}
-        {state.matches('Creating new todo.Showing form input') && (
-          <input onChange={e => {
-            send({
-              type: "Form input changed",
-              value: e.target.value
+              type: "Submit"
             })
           }}>
-          </input>
+            <input
+              onChange={(e) => {
+                send({
+                  type: "Form input changed",
+                  value: e.target.value,
+                });
+              }}
+            ></input>
+          </form>
         )}
       </div>
     </div>
-  )
+  );
 }
