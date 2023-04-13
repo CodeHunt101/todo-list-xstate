0650ca11f25ad7e9819ce4d75ab41de39bd1b1bd
Harold <harold.marino@iag.com.au>
2023-04-13 11:31:18 +1000

    adding todo item

--- machines/todoAppMachine.ts
+++ machines/todoAppMachine.ts
@@ -2,7 +2,7 @@ import { actions, assign, createMachine } from "xstate";
 
 export const todosMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogAsAdgoBmJwDY3ATnv2PAJgCsHl4ANCAAnogAtACMrq4U0Voe-vZJtk4AHK4errYAvnmhQjgEJJwS9KSMQmxgAE51qHUURgA2KgBmTdgUxSJl4rSV1TJyCkrmmrr6liawZmqkljYIDs5unt5+gSHhUQEeFP6uvtmB6Vk5+YUgfaVivaOYFZCsAMJ1YCpgmOQA7jMkCA5gsLECVr4nAlbBlLlsDjkMqEIghfLZDk5fFporF-Mdok4tG4CkUZCVRJwPl9VFVfmA-phlDIKABlQioP5SLp1bCYMhGACuylYADFunzSILlJgiLgqpBAcZTJNlohokEKL4cZsfAj7MjEFqMhQMuk8bYtPjCcSSSBSOg4JY7hSwLNlYtVQgYjlNbZovY8b4sfYLUSDV6nGstCdsukHNaPE5bc6BtQhlIam75irwVFojDff7A8HQ05w5E0fEMm4CZGLtlcsmyf0HjVnkNIFnQUtc17a4WA-4g1oQ1ow3tUeiTVica48a4CUTXE2MOTUxUpEyWJh6o1PhAuznQCsYpCB8WR6Xw-mMWb-BarUuV8J7pTPiopP9GTJDx7e9F-GiTVtWyeEdn1CcCSAyMnHNS0F2tZcbhTB4qQ-Wkvy3VBWXZTlaW5Xl+SFX8wWPQ0nCA+MtFhIdtiCCCUUxLRNRyVwfHgxcbQKIA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogC0ATgBsFAMwBWAIxvHWgBwAWAHZfRx9AgBoQAE87D3tfCi0Xey1Hf2T7F38tNwBfXMihHAISTgl6UkYhNjAAJ1rUWoojABsVADNG7AoikVLxWgqqmTkFJXNNXX1LE1gzNVJLGwQHZ3cvHwDg0K0I6LsAJnsPCn97A49fFKdvN0D8wpli0U5qzHLIVgBhWrAVMEw5AA7tMkCBZvMLGDlrYPActBQ3MktGc0iErr5fJEYggDoEER5soEjnE3H4Qg8QL0SmIKD8-qpKoCwEDMMoZBQAMqEVBAqSdWrYTBkIwAV2UrAAYl1haQxcpMERcJVIKDjKYJktEHDAhQDi4Ub5Ao4rscXAcsfsVoSTgczvZifZPL5CfcClSnn1afSVFJgWyOdzefyZSLxaxOaKAEbYMxq8EahZahBpfx6y7G43m-yODx7HEOBEok1BJ2XV2U6kvSg+xmMf3sjBc3DcKSN1DsdSUBQCHqemmcWt+lkBpuclttmTyUi8cYLfTxiGa6GxLQHRH4g1+PyOc1ObGIFyBE4hQIuDwmzyBY2Eyv96t036+pkNwMTpnt1h1BpNVodLp9hgzz9I+DLDqy7bNq2H5TmMvrqAuugzImUKgMsx7OBsFouL47jZHmB4IHmCRaJkmJHA6xJaB4Lj5O6pDoHAlhVv0yFzMuaF2EiLgUL4Wi7C6BxuIRthHgk9jnJc1yOLcbqPEBXplIMUjVGxkKLCuKwkhQ5xOEawlWrYeIJMa3jmo4drZKkd4KQOggjO8gyQGpHHWHY-i+G4vH8cEcIGTiFxeYETgOscTrkhZNnCHZ1DKTBLCYN+jTOWCS5JppRmhDpRwmoE-nanmpyEqRHiEkEnjUVFwHek+dbMhBMguelnEIMFeoGh5xqmjRFoiQcFoUIE9qOs6FbuixNVgS+I6QUGfJMgKQphsoTWoW5CDns4RpXP4NHnJ5LiHYRmKuBe5W+EJHluBcVWKTWtXgaOqBQZOGCrRpLXlbx7hOp42a5qEhGHWmunBQcuYuI4x6+HRuRAA */
     tsTypes: {} as import("./todoAppMachine.typegen").Typegen0,
     id: "Todo machine",
     initial: "Loading Todos",
@@ -11,9 +11,14 @@ export const todosMachine = createMachine(
         loadTodos: {
           data: string[];
         };
+        saveTodo: {
+          data: void
+        }
       },
       events: {} as {
         type: 'Create new'
+      } | {
+        type: 'Submit'
       } | {
         type: 'Form input changed',
         value: string
@@ -58,9 +63,24 @@ export const todosMachine = createMachine(
             on: {
               "Form input changed": {
                 actions: "assignFormInputFormToContext"
-              }
+              },
+
+              Submit: "Saving todo"
             }
           },
+
+          "Saving todo": {
+            invoke: {
+              src: "saveTodo",
+
+              onError: {
+                target: "Showing form input",
+                actions: 'assignErrorToContext'
+              },
+
+              onDone: "#Todo machine.Loading Todos"
+            }
+          }
         },
       }
     },
