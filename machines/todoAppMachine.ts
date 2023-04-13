df4bf9497be9532905be24995c9058d568ca5e5b
Harold <harold.marino@iag.com.au>
2023-04-13 14:16:08 +1000

    deleting a todo item

--- machines/todoAppMachine.ts
+++ machines/todoAppMachine.ts
@@ -2,7 +2,7 @@ import { actions, assign, createMachine } from "xstate";
 
 export const todosMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogC0ATgBsFAMwBWAIxvHWgBwAWAHZfRx9AgBoQAE87D3tfCi0Xey1Hf2T7F38tNwBfXMihHAISTgl6UkYhNjAAJ1rUWoojABsVADNG7AoikVLxWgqqmTkFJXNNXX1LE1gzNVJLGwQHZ3cvHwDg0K0I6LsAJnsPCn97A49fFKdvN0D8wpli0U5qzHLIVgBhWrAVMEw5AA7tMkCBZvMLGDlrYPActBQ3MktGc0iErr5fJEYggDoEER5soEjnE3H4Qg8QL0SmIKD8-qpKoCwEDMMoZBQAMqEVBAqSdWrYTBkIwAV2UrAAYl1haQxcpMERcJVIKDjKYJktEHDAhQDi4Ub5Ao4rscXAcsfsVoSTgczvZifZPL5CfcClSnn1afSVFJgWyOdzefyZSLxaxOaKAEbYMxq8EahZahBpfx6y7G43m-yODx7HEOBEok1BJ2XV2U6kvSg+xmMf3sjBc3DcKSN1DsdSUBQCHqemmcWt+lkBpuclttmTyUi8cYLfTxiGa6GxLQHRH4g1+PyOc1ObGIFyBE4hQIuDwmzyBY2Eyv96t036+pkNwMTpnt1h1BpNVodLp9hgzz9I+DLDqy7bNq2H5TmMvrqAuugzImUKgMsx7OBsFouL47jZHmB4IHmCRaJkmJHA6xJaB4Lj5O6pDoHAlhVv0yFzMuaF2EiLgUL4Wi7C6BxuIRthHgk9jnJc1yOLcbqPEBXplIMUjVGxkKLCuKwkhQ5xOEawlWrYeIJMa3jmo4drZKkd4KQOggjO8gyQGpHHWHY-i+G4vH8cEcIGTiFxeYETgOscTrkhZNnCHZ1DKTBLCYN+jTOWCS5JppRmhDpRwmoE-nanmpyEqRHiEkEnjUVFwHek+dbMhBMguelnEIMFeoGh5xqmjRFoiQcFoUIE9qOs6FbuixNVgS+I6QUGfJMgKQphsoTWoW5CDns4RpXP4NHnJ5LiHYRmKuBe5W+EJHluBcVWKTWtXgaOqBQZOGCrRpLXlbx7hOp42a5qEhGHWmunBQcuYuI4x6+HRuRAA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALradiUAAdZxVesMgAHogC0ATgBsFAMwBWAIxvHWgBwAWAHZfRx9AgBoQAE87D3tfCi0Xey1Hf2T7F38tNwBfXMihHAISTgl6UkYhNjAAJ1rUWoojABsVADNG7AoikVLxWgqqmTkFJXNNXX1LE1gzNVJLGwQHZ3cvHwDg0K0I6LsAJnsPCn97A49fFKdvN0D8wpli0U5qzHLIVgBhWrAVMEw5AA7tMkCBZvMLGDlrYPActBQ3MktGc0iErr5fJEYggDoEER5soEjnE3H4Qg8QL0SmIeiN3oNPgARMAtMDKMCg4ymCZLOw+ZyOA4uEJaLy+QIijzYxB4hJHRz2QLHexkzGOSnUl6UH5-VSVQFgIGYZQyCgAZUIqCBUk6tWwmDIRgArspWAAxLqO0gu5SYIi4SqQLngnkLPkIOGBCjClESxxXY4uA5Y-YrQknA5nJVHTy+Qn3ApUp59Wm6lRSYEms2W622r1O12sc3OgBG2DMIYhvOhiDS-hjl0CjmHyf8jg8exxDgRKITQVVlwLmpLNM45f1jCrpowFtw3CkO9Q7HUlAUAjpGGe-QoG8rRuru-N+8PMnkpF44wW+i7YahoBhDwtAORF8RcLQ-D8RxkycGUEBcQIThCSUPATTxAmHQkVyvUt11+CsDW3GsXwNI9WDqBomlaDoukvYQ1x1fDN0NY0jz3A9SLfMYK3UH9dBmP9Fl7BBEOcDYUxFdxsknODJwSLRMkxI4lWJMUXGw+jtQoFk2WYsiKMaZo2mUO1ui1G8dPZV8MHfT8eMmPR+LBbtw2E2x-H8EDFxybwQkcMlMjgjyXAoYkXB8PEDjcA4JzyItzNpSy9JkE9OHPQRVy0pLrNQWzFHsviDGcwSI3cjzTizNEySSSVHDgvEQsXfw3F8FqgnAilKVIdA4EsBLyAEuYewA-k3AHIURR8cVJXzODbAQhJ7HOS4pXAsUNOvWlyikapBshISRpWEkKHOJwJTcOa5VC-zoJirNslSDbcIylgGToSA9uG6w7DGk5Lk8Dz0hyOFUxxS4EWVfyIJWlwgKSJ6GOoQYctgTADN+CBPtcw7bBi5xToTQILrTDxJ1OQkFNJjyiaAjwEa0u9CIfI8sf-b6RPsGNwP8eNEw8ZNQcOFNQuzEJvGVDD6ZvRmt2ZmsrRtA1TO9X1WYO9nYecCUrn8fnzlalxDbgzFXFQoIWqinnorp+LMulpj71Y4iOMYFniqG7H2fNihfHcVVPDHCdQjgw2B1O5UYv5kdLilxLWSsziMDV0qecFYVRWmkVdhDw2ToBlq2slKDY84bLE6wdGPvd-bSoOGKKALaL0OOJFoqCzyY2FfsI5FfENXyXIgA */
     tsTypes: {} as import("./todoAppMachine.typegen").Typegen0,
     id: "Todo machine",
     initial: "Loading Todos",
@@ -13,6 +13,9 @@ export const todosMachine = createMachine(
         };
         saveTodo: {
           data: void
+        },
+        deleteTodo: {
+          data: void
         }
       },
       events: {} as {
@@ -22,6 +25,9 @@ export const todosMachine = createMachine(
       } | {
         type: 'Form input changed',
         value: string
+      } | {
+        type: 'Delete',
+        todo: string
       }
     },
     context: {
@@ -50,7 +56,8 @@ export const todosMachine = createMachine(
 
       "Todos Loaded": {
         on: {
-          "Create new": "Creating new todo"
+          "Create new": "Creating new todo",
+          Delete: "Deleting todo"
         }
       },
 
@@ -82,7 +89,17 @@ export const todosMachine = createMachine(
             }
           }
         },
-      }
+      },
+
+      "Deleting todo": {
+        invoke: {
+          src: 'deleteTodo',
+          onError: {target: "Deleting todo errored", actions: "assignErrorToContext"},
+          onDone: "#Todo machine.Loading Todos"
+        }
+      },
+
+      "Deleting todo errored": {}
     },
   },
   {
